777b9300cdaed32d48f99e31651bd012d8df8f0a
Nikita Kornauhov (laptop) <lohar.nomer@yandex.ru>
2024-02-16 13:17:42 +0400

    Первый Ðкоммит с шаблоном + configÐ

---
+++ lexicon/lexicon.py
@@ -0,0 +1,13 @@
+class LEXICON_RU:
+    start = ''
+    continue_ = ''
+    help = ''
+    left = '<<<'
+    right = '>>>'
+    error = ''
+
+class LEXICON_COMMANDS_RU:
+    start = 'start', 'запустить бота'
+    help = 'help', ''
+    continue_ = 'continue', 'продолжить с той страницы, на которой остановились'
+    
---
+++ servises/file_handling.py
@@ -0,0 +1,23 @@
+book: dict[int, str] = {}
+PAGE_SIZE = 100
+
+
+def prepare_book(path: str) -> None:
+    page = 1
+    with open(path, 'r', encoding='UTF-8') as file:
+        text = file.read()
+        start = 0
+        end = len(text)
+        while start < end:
+            page_text, current_page_size = _get_part_text(text, start, PAGE_SIZE)
+            start += current_page_size + 1
+            book[page] = page_text.lstrip()
+            page += 1
+
+def _get_part_text(text, start, page_size):
+    end = start + page_size - 1
+    stop = ',!?.:;'
+    while len(text) > end and (text[end] not in stop or text[end] in stop and (text[end-1] in stop or text[end+1] in stop)):
+        end -= 1
+    return text[start:end + 1], len(text[start:end + 1])
+
