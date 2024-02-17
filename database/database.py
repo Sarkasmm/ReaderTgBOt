6332daa88de274cc9543f3f7b2c2ec3ce73c02d7
Nikita Kornauhov (laptop) <lohar.nomer@yandex.ru>
2024-02-17 14:15:09 +0400

    Комит полеследний

--- database/database.py
+++ database/database.py
@@ -1,4 +1,12 @@
-template = {
-    'page': 1,
-    'bookmarks': set()
-}
+class Users:
+    users_db = {}
+
+    def __init__(self, user_id):
+        if user_id not in self.users_db:
+            self.user_id = user_id
+            self.page = 0
+            self.bookmarks = set()
+
+    def append_bookmarks(self, bookmark):
+        self.users_db[self.user_id]] = 0
+
